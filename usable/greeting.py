71a8282ac0d854a3ef5c578d9a166921c02c0893
Valejo Johnson <valejo@VJs-Mac-Studio>
2024-06-27 15:20:54 -0700

    updating greeting.py with more comments

--- usable/greeting.py
+++ usable/greeting.py
@@ -1,8 +1,14 @@
+# This python code is designed to greet a user
+# the proper salutation based on local time
+
+# Importing datetime as this will help us get the hour of day
 from datetime import datetime
 
-
+# Function to get the time of day and if hour
+# is less than Noon print Good Morning
+# else print Good Evening
 def get_time():
-    # Get the current local hour
+    # Get the current local time
     current_hour = datetime.now().time()
 
     # If before Noon, return Good Morning, anything else Good Evening
