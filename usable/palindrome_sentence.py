a4aa011ef6cc11600e2684b436bc26e6af97e23b
Valejo Johnson <valejo@VJs-Mac-Studio>
2024-06-27 15:49:13 -0700

    adding palindome_sentence.py to usable

--- usable/palindrome_sentence.py
+++ usable/palindrome_sentence.py
@@ -1,3 +1,9 @@
+# This python code is designed to test if a sentence
+# is a palindrome or not
+
+# Function to generate a new sentence based on input
+# by removing all but letters, then testing if it matches
+# as palindrome
 def is_palindrome(sentence):
     # Will need to create a new sentence as we want to ignore non-alphanumeric characters
     new_sentence = ''
@@ -11,10 +17,17 @@ def is_palindrome(sentence):
     result = new_sentence[::-1]
 
     if new_sentence.casefold() == result.casefold():
-        print("Sentence is Palindrome")
+        print(f"The sentence {sentence} IS Palindrome")
     else:
-        print("Sentence is NOT palindrome")
+        print(f"The sentence {sentence} is NOT palindrome")
 
 
+# Taking input from user no matter the case or characters used
 sentence = input("What sentence would you like to test? ")
 is_palindrome(sentence)
+
+
+# Sentences to test
+# Do Geese See God?
+# Was it a car or a cat I saw?
+# Mr. Owl ate my metal worm
