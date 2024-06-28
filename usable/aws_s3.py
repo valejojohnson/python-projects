f526443ed8fd60e7a923c80b7920147474c5e0f2
Valejo Johnson <valejo@VJs-Mac-Studio>
2024-06-28 16:14:27 -0700

    moving aws_s3.py to usable

--- usable/aws_s3.py
+++ usable/aws_s3.py
@@ -1,19 +1,29 @@
+# This python code is designed to take input from user on a bucket
+# name to create. If bucket already exists, ask they'd like to keep it.
+# Else, it will create the bucket.
+
 import time
 import boto3
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
+s3 = boto3.client('s3')
 
 
 def check_s3_bucket(bucket):
     try:
         s3.head_bucket(Bucket=bucket)
-        return True
+        print()
+        print(f"S3 Bucket already exists: {bucket}")
+        print()
+        answer = input(f'Would you like to delete {bucket}? (Y/N)')
+        if answer == 'Y':
+            delete_s3_bucket(bucket)
+
     except ClientError as e:
         if e.response['Error']['Code'] == '404':
-            print(f"The bucket {bucket} DOESN'T exist in this account")
+            print(f"The bucket {bucket} DOESN'T already exist")
             print()
             print(f'creating bucket {bucket}..')
-            print()
-            return False
+            create_s3_bucket(bucket)
         else:
             raise
 
@@ -22,20 +32,17 @@ def create_s3_bucket(bucket):
     try:
         s3.create_bucket(Bucket=bucket)
         print(f'Bucket {bucket} successfully created')
-        return True
     except ClientError as e:
         print(f'There was an error creating the bucket: {e}')
-        return False
 
 
 def delete_s3_bucket(bucket):
     try:
         s3.delete_bucket(Bucket=bucket)
         print()
-        print(f"Bucket {bucket} has been removed.")
+        print(f"Bucket {bucket} has been destroyed.")
     except ClientError as e:
         print(f"There was a problem deleting {bucket}: {e}")
-        return False
 
 
 def list_s3_buckets():
@@ -48,15 +55,5 @@ def list_s3_buckets():
         print(name)
 
 
-s3 = boto3.client('s3')
-bucket = input('Which bucket would you like to check?: ')
-if not check_s3_bucket(bucket):
-    create_s3_bucket(bucket)
-    print('Listing Existing Buckets: ')
-    list_s3_buckets()
-    time.sleep(10)
-    print(f"Deleting bucket {bucket}")
-    delete_s3_bucket(bucket)
-else:
-    print(f"Bucket {bucket} already present")
-
+bucket = input('Which bucket would you like to create?: ')
+check_s3_bucket(bucket)
