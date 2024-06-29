5aa5cca423119e86af93aa745c5dc16e1b2adab4
Valejo Johnson <valejo@VJs-Mac-Studio>
2024-06-28 22:58:16 -0700

    adding maxlist.py to learning

---
+++ learning/maxlist.py
@@ -0,0 +1,13 @@
+# This is designed to add a character to
+# a list until a maximum count
+
+def add_to_list():
+    trash = ['i']
+    maximum = 11
+
+    while len(trash) < maximum:
+        trash.append("i")
+        print(trash)
+
+
+add_to_list()
