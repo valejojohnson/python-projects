5b0484de3a2ab6c703647921b48899dbceccba82
Valejo Johnson <valejo@VJs-Mac-Studio>
2024-06-28 18:20:05 -0700

    updates aws_s3.py

--- usable/aws_s3.py
+++ usable/aws_s3.py
@@ -6,10 +6,9 @@ import boto3
 from botocore.exceptions import NoCredentialsError, ClientError
 from colorama import Fore, Style, init
 
-# Constant Variables
+# Initialize Colorama
 init(autoreset=True)  # This is to constantly reset the color after every run
 s3 = boto3.client('s3')  # Don't feel like typing out boto.client('s3') everytime
-response = True  # This is to run the program until it's False
 
 
 def check_s3_bucket(bucket):
@@ -18,14 +17,14 @@ def check_s3_bucket(bucket):
         print()
         print(Fore.YELLOW + f"S3 Bucket already exists: {bucket}")
         print()
-        answer = input(f'Would you like to delete {bucket}? (Y/N) ')
-        if answer == 'Y'.casefold():
+        answer = input(f'Would you like to delete {bucket}? (Y/N) ').strip().lower()
+        if answer == 'y':
             delete_s3_bucket(bucket)
 
     except ClientError as e:
         if e.response['Error']['Code'] == '404':
 
-            print(Fore.RED + f"The bucket {bucket} DOESN'T already exist")
+            print(Fore.RED + f"The bucket {bucket} DOESN'T exist.")
             print()
             create_s3_bucket(bucket)
         else:
@@ -34,12 +33,11 @@ def check_s3_bucket(bucket):
 
 def create_s3_bucket(bucket):
     try:
-        print()
         print(f'Creating bucket {bucket}...')
         s3.create_bucket(Bucket=bucket)
         print(Fore.GREEN + f'Bucket {bucket} successfully created')
-        question = input('Would you like to list all buckets? (Y/N)')
-        if question == 'Y'.casefold():
+        question = input('Would you like to list all buckets? (Y/N)').strip().lower()
+        if question == 'Y':
             list_s3_buckets()
     except ClientError as e:
         print(f'There was an error creating the bucket: {e}')
@@ -52,16 +50,19 @@ def delete_s3_bucket(bucket):
         print(f"S3 Bucket has been destroyed: {bucket} ")
         print()
     except ClientError as e:
-        print(f"There was a problem deleting {bucket}: {e}")
+        print(f"There was a problem destroying {bucket}: {e}")
 
 
 def list_s3_buckets():
-    listing = s3.list_buckets()
-
-    bucket_names = [bucket['Name'] for bucket in listing['Buckets']]
-    for name in bucket_names:
-        print()
-        print(name)
+    # List all S3 buckets.
+    try:
+        response = s3.list_buckets()
+        bucket_names = [bucket['Name'] for bucket in response['Buckets']]
+        print("Buckets:")
+        for name in bucket_names:
+            print(name)
+    except ClientError as e:
+        print(f"Error listing buckets: {e}")
 
 
 def check_credentials(bucket):
@@ -76,19 +77,28 @@ def check_credentials(bucket):
     except ClientError as e:
         error_code = e.response['Error']['Code']
         if error_code == 'AccessDenied':
-            print(f"Access is denied. Check credentials")
+            print(f"Access is denied. Check your credentials")
         else:
             print(f"An error occurred: {e}")
 
 
-while response is not False:
-    print()
-    response = input("Would you like to create a new bucket (Y/N) ")
-    if response == 'y'.lower() or response == 'Y':
+def main():
+    response = True  # This is to run the program until it returns False
+
+    while response:
         print()
-        bucket = input('Name the bucket to create?: ')
-        check_credentials(bucket)
-    else:
-        print()
-        print(Fore.RED + 'Exiting Program.')
-        response = False
+        response = input("Would you like to create a new bucket (Y/N) ").strip().lower()
+        if response == 'y':
+            print()
+            bucket = input('Name the bucket to create?: ').strip()
+            check_credentials(bucket)
+        elif response == 'n':
+            print(Fore.RED + 'Exiting Program')
+            response = False
+        else:
+            print()
+            print(Fore.YELLOW + "Invalid input. Please enter 'Y' or 'N'. ")
+
+
+if __name__ == '__main__':
+    main()
