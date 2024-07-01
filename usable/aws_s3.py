0821e1c4f8279b7a5695d63c2046784817299ec2
VJohnson1 <vjohnson@williamhill.us>
2024-06-30 17:58:53 -0700

    updating aws_s3.py

--- usable/aws_s3.py
+++ usable/aws_s3.py
@@ -8,14 +8,25 @@ from colorama import Fore, Style, init
 
 # Initialize Colorama
 init(autoreset=True)  # This is to constantly reset the color after every run
-s3 = boto3.client('s3')  # Don't feel like typing out boto.client('s3') everytime
+region = 'us-east-1'
+s3 = boto3.client('s3', region_name=region)  # Don't feel like typing out boto.client('s3') everytime
+
+
+def check_items__in_bucket(bucket):
+    if s3.list_objects_v2(Bucket=bucket) != '':
+        return True  # Meaning bucket isn't empty
+    else:
+        return False
 
 
 def check_s3_bucket(bucket):
     try:
-        s3.head_bucket(Bucket=bucket)
-        print()
-        print(Fore.YELLOW + f"S3 Bucket already exists: {bucket}")
+        if s3.head_bucket(Bucket=bucket):
+            check_items__in_bucket(bucket)
+            if check_items__in_bucket(bucket) is True:
+                print(Fore.YELLOW + f"S3 Bucket already exists but it's not empty: {bucket}")
+            else:
+                print(Fore.YELLOW + f"S3 Bucket already exists but it's empty: {bucket}")
         print()
         answer = input(f'Would you like to delete {bucket}? (Y/N) ').strip().lower()
         if answer == 'y':
@@ -31,11 +42,18 @@ def check_s3_bucket(bucket):
             raise
 
 
-def create_s3_bucket(bucket):
+def create_s3_bucket(bucket, region):
     try:
-        print(f'Creating bucket: {bucket}')
-        s3.create_bucket(Bucket=bucket)
-        print(Fore.GREEN + f'Bucket {bucket} successfully created')
+        if region is None or region == 'us-east-1':
+            location = {'LocationConstraint': 'us-east-1'}
+            print(f'Creating bucket in US-EAST-1: {bucket}')
+            s3.create_bucket(Bucket=bucket, CreateBucketConfiguration=location)
+            print(Fore.GREEN + f'Bucket {bucket} successfully created')
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3.create_bucket(Bucket=bucket, CreateBucketConfiguration=location)
+            print(Fore.GREEN + f'Bucket {bucket} successfully created in {region}')
         question = input('Would you like to list all buckets? (Y/N)').strip().lower()
         if question == 'y':
             list_s3_buckets()
@@ -82,14 +100,15 @@ def check_credentials(bucket):
             print(Fore.RED + f"An error occurred: {e}")
 
 
-def main():
+def main(region):
     while True:
+
         print()
         response = input("Would you like to check/create a new bucket (Y/N) ").strip().lower()
         if response == 'y':
             print()
             bucket = input('Name the bucket to check/create?: ').strip().lower()
-            check_credentials(bucket)
+            check_credentials(bucket, region)
         elif response == 'n':
             print(Fore.RED + 'Exiting Program')
             return False
@@ -99,4 +118,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main(region)
