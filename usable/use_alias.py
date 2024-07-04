ea65587216ccddeff9e29baa522cf8abd60af880
VJohnson1 <vjohnson@williamhill.us>
2024-07-04 09:23:39 -0700

    updating aliasname.py

--- usable/use_alias.py
+++ usable/use_alias.py
@@ -1,21 +1,26 @@
 # This code is to use an alias name if present
-# if no alias, just use regular name
+# if no alias, just use regular first name
 
 
 def get_input(prompt):
     return input(prompt).strip().capitalize()
 
 
-first_name = get_input("What's your first name? ")
-print()
+def main():
+    first_name = get_input("What's your first name? ")
+    print()
 
-last_name = get_input(f"Hi {first_name}, what's your last name? ")
-print()
+    last_name = get_input(f"Hi {first_name}, what's your last name? ")
+    print()
 
-alias = get_input("Do you have a nickname you like to be called? ")
-print()
+    alias = get_input("Do you have a nickname you like to be called? ")
+    print()
 
-if not alias:
-    print(f"Hi {first_name} {last_name}. We're happy to have you here")
-else:
-    print(f"Hi {alias} {last_name}. We're happy to have you here")
+    if not alias:
+        print(f"Hi {first_name} {last_name}. We're happy to have you here")
+    else:
+        print(f"Hi {alias} {last_name}. We're happy to have you here")
+
+
+if __name__ == "__main__":
+    main()
