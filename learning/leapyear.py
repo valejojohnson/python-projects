0365615cac80e016cd1aaa411d14561a7854125a
VJohnson1 <vjohnson@williamhill.us>
2024-07-07 21:28:16 -0700

    adding leapyear.py

---
+++ learning/leapyear.py
@@ -0,0 +1,12 @@
+#year = input("Input a year to test: e.g (YYYY) \n")
+
+year = 2024
+
+def isleap(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(f"{year} is a leap year")
+    else:
+        print(f"{year} is not a leap year")
+
+
+isleap(year)
